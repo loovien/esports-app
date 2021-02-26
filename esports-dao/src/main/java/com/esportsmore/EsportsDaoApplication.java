689a45b614c145c92f2d21ee254a2c885c495d44
luowen <bigpao.luo@gmail.com>
2021-02-26 18:09:48 +0800

    change

--- esports-dao/src/main/java/com/esportsmore/EsportsDaoApplication.java
+++ esports-dao/src/main/java/com/esportsmore/EsportsDaoApplication.java
@@ -1,9 +1,12 @@
-package com.esportsmore.dao;
+package com.esportsmore;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.esportsmore.dao"})
 public class EsportsDaoApplication {
 
     public static void main(String[] args) {
